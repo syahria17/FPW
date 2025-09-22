3a67f81358f284fe27df6c8ec48e774ea425fcf2
syahria17 <syahriata1717@gmail.com>
2025-09-22 23:36:41 +0700

    done

---
+++ Praktikum/resources/views/product.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Page</title>
+</head>
+<body>
+    <h1>Hasil Penjumlahan</h1>
+    <p>Angka: {{ $angka }}</p>
+</body>
+</html>
--- Praktikum/routes/web.php
+++ Praktikum/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/rahasia', function(): string{
+    return 'ini path rahasia';
+})-> middleware(['auth', 'rollcheck:admin']);
+
+Route::get('/product/{number}', [ProductController::class, 'index'])
+    ->middleware(['auth','rollcheck:admin,owner'])
+    ->name('product.index');
 
 require __DIR__.'/auth.php';
